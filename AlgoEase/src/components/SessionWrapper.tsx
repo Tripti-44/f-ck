a6e2119b49004aa3d1a6e449e2aa0819574fa9dd
Tripti <44tripti@gmail.com>
2026-02-14 23:31:37 +0530

    Keep only Bloom-Quest project

--- AlgoEase/src/components/SessionWrapper.tsx
+++
@@ -1,7 +0,0 @@
-// "use client"; // ✅ This ensures it's a Client Component
-
-// import { SessionProvider } from "next-auth/react";
-
-// export default function SessionWrapper({ children }: { children: React.ReactNode }) {
-//   return <SessionProvider>{children}</SessionProvider>;
-// }
